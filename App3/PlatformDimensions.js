a38c7b4af0b0ec3cb022dddde0c31f0447510dd8
mattperkins <github@mattperkins.com>
2018-06-27 10:31:08 +0100

    cl

--- App3/PlatformDimensions.js
+++ App3/PlatformDimensions.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { View, Text, Platform } from 'react-native'
 
 export default class PlatformDimensions extends React.Component{
+
+    state={
+        os: Platform.OS
+    }
+
     render(){
+        
         return (
             <View>
                 <Text>
                 { 
-                    Platform.OS === 'ios' ? 
-                        'Welcome to IOS' :
+                   this.state.os  === 'ios' ? 
+                        'Welcome to IOS!' :
                         'Welcome to Android'
                 }
                 </Text>
