5d4acd89303e82fd58aab550d3d131ebe9726f25
mattperkins <github@mattperkins.com>
2018-07-01 18:53:36 +0100

    add animations

--- App4/Animator.js
+++ App4/Animator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Animated } from 'react-native'
+import { View, Animated, Easing } from 'react-native'
 
 export default class App4 extends React.Component{
 
@@ -7,10 +7,19 @@ export default class App4 extends React.Component{
     super(props)
    
     this.redSquare = new Animated.ValueXY(0,0)
+    this.blueSquare = new Animated.ValueXY(0,0)
 
         Animated.spring(this.redSquare, {
             toValue:{ x:100, y:300 },
-            friction: 1
+            friction: 1,
+            delay: 1000
+        }).start()
+
+        Animated.timing(this.blueSquare, {
+            toValue:{ x:-100, y:100 },
+            duration: 5000,
+            delay: 2000,
+            easing: Easing.bounce
         }).start()
 
     }
@@ -29,6 +38,18 @@ export default class App4 extends React.Component{
                     style={this.redSquare.getLayout()}
                 >
                     <View style={{backgroundColor: 'red', width: 40, height: 40}}></View>
+                </Animated.View>
+
+                <Animated.View
+                    // style={{
+                    //     left: this.redSquare.x,
+                    //     top: this.redSquare.y
+                    // }}
+
+                    // same as above - but specific to ValueXY operation
+                    style={this.blueSquare.getLayout()}
+                >
+                    <View style={{backgroundColor: 'blue', width: 40, height: 40}}></View>
                 </Animated.View>
 
             </View>
