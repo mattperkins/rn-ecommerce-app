31ed1ded4142a829bf536aea6770d06a134d94f5
mattperkins <github@mattperkins.com>
2018-07-01 13:51:51 +0100

    add friction to animation

--- App4/Animator.js
+++ App4/Animator.js
@@ -9,9 +9,8 @@ export default class App4 extends React.Component{
     this.redSquare = new Animated.ValueXY(0,0)
 
         Animated.spring(this.redSquare, {
-            toValue:{
-                x:100, y:300
-            }
+            toValue:{ x:100, y:300 },
+            friction: 1
         }).start()
 
     }
@@ -25,7 +24,7 @@ export default class App4 extends React.Component{
                     //     left: this.redSquare.x,
                     //     top: this.redSquare.y
                     // }}
-                    
+
                     // same as above - but specific to ValueXY operation
                     style={this.redSquare.getLayout()}
                 >
