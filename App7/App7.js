81e1c5bd37aae9859f27da62b16c7737d99dd7f9
mattperkins <github@mattperkins.com>
2018-07-02 11:11:00 +0100

    prep for redux

---
+++ App7/App7.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class App7 extends React.Component {
+ 
+  render() {
+ 
+   return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <Text>Hello, World!</Text>
+    </View>
+)
+}
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native'
-import App6 from './App6/App6'
+import App7 from './App7/App7'
 import { YellowBox } from 'react-native'
 YellowBox.ignoreWarnings (
     [ 'Warning: isMounted(...) is deprecated', 'Module RCTImageLoader' ]
 )
-AppRegistry.registerComponent('space_monkey', () => App6)
+AppRegistry.registerComponent('space_monkey', () => App7)
