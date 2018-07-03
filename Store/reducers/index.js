36007da0d2db8910defeb2cb0cd676de517b4940
mattperkins <github@mattperkins.com>
2018-07-03 16:52:23 +0100

    purge and restructure project directories

--- Store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import users from './user_reducer'
-
-const rootReducer = combineReducers({
-    users
-})
-
-export default rootReducer
--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 import React from 'react'
+import { Text, View } from 'react-native'
+
 import { AppRegistry } from 'react-native'
 
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import promiseMiddleware from 'redux-promise'
-
-import reducers from './Store/reducers'
-
-import App7 from './App7/App7'
 import { YellowBox } from 'react-native'
 YellowBox.ignoreWarnings (
     [ 'Warning: isMounted(...) is deprecated', 'Module RCTImageLoader' ]
 )
 
-const createStoreWithMiddleWare = applyMiddleware(promiseMiddleware)(createStore)
 
-const AppRedux = () => (
-    <Provider store={createStoreWithMiddleWare(reducers)}>
-        <App7 />
-    </Provider>
+
+const App = () => (
+  
+        <View>
+            <Text>Hello, World!</Text>
+        </View>
+  
 )
 
 
 
-AppRegistry.registerComponent('space_monkey', () => AppRedux)
+AppRegistry.registerComponent('space_monkey', () => App)
