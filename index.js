afb35ff3a0092564aec67b4f1818d8a3e89d3c61
mattperkins <github@mattperkins.com>
2018-07-05 01:45:55 +0100

    add card side swipe functionality

--- index.js
+++ index.js
@@ -19,11 +19,90 @@ const Users = [
 
 export default class App extends Component {
  
+ constructor(props){
+     super(props)
+
+    this.position = new Animated.ValueXY()
+    this.state = {
+        currentIndex: 0
+    }
+
+    this.rotate = this.position.x.interpolate({
+        inputRange:[-SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2],
+        outputRange:['-10deg', '0deg', '10deg'],
+        extrapolate: 'clamp'
+    })
+
+    this.rotateAndTranslate = {
+        transform:[{
+            rotate: this.rotate
+        },
+        ...this.position.getTranslateTransform()
+        ]
+    }
+
+     this.PanResponder = PanResponder.create({
+
+         onStartShouldSetPanResponder:(evt, gestureState) => true,
+         onPanResponderMove:(evt, gestureState) => {
+             this.position.setValue({
+                 x: gestureState.dx,
+                 y: gestureState.dy
+             })
+         },
+        onPanResponderRelease:(evt, gestureState) => {
+            
+        }
+     })
+ }
+
+
  renderUsers = () => {
 
      return Users.map((item,i) => {
-         return (
+
+         if( i < this.state.currentIndex ) {
+             return null
+         }
+         else if (i === this.state.currentIndex) {
+             return (
             <Animated.View 
+            {...this.PanResponder.panHandlers}
+                key={item.id}
+                style={[this.rotateAndTranslate, { height:SCREEN_HEIGHT-120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' } ]}>
+
+                {/* DOPE BUTTON */}
+                <Animated.View 
+                    style={{
+                        transform: [{rotate: '-30deg'}], position: 'absolute', top:50, left:50, zIndex:1000
+                    }}>
+                    <Text style={{
+                        borderWidth:1, borderColor:'green', color:'green', fontSize:32, fontWeight:'800', padding:10  
+                    }}>DOPE</Text>
+                </Animated.View>
+
+
+                {/* NOPE BUTTON */}
+                <Animated.View 
+                    style={{
+                        transform: [{rotate: '30deg'}], position: 'absolute', top:50, left:50, zIndex:1000
+                    }}>
+                    <Text style={{
+                        borderWidth:1, borderColor:'green', color:'green', fontSize:32, fontWeight:'800', padding:10  
+                    }}>NOPE</Text>
+                </Animated.View>
+
+                <Image
+                    style={{ flex:1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }} 
+                    source={item.lemon} />
+
+            </Animated.View>
+         )
+         }
+         else {
+             return (
+            <Animated.View 
+          
                 key={item.id}
                 style={{height:SCREEN_HEIGHT-120, width: SCREEN_WIDTH, padding: 10, position: 'absolute'}}>
                 <Image
@@ -31,6 +110,7 @@ export default class App extends Component {
                     source={item.lemon} />
             </Animated.View>
          )
+         }
      }).reverse()
 
  }
