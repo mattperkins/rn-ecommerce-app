c7f0a8a1206c1eb75a3a9240ed96d4d8cd56fb06
mattperkins <github@mattperkins.com>
2018-07-05 01:01:05 +0100

    add swipe cards UI

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 import { AppRegistry } from 'react-native'
-import React from 'react'
-import { Text, View } from 'react-native'
-
+import React, { Component } from 'react'
+import { Text, View, Image, Dimensions, Animated } from 'react-native'
 
 import { YellowBox } from 'react-native'
 YellowBox.ignoreWarnings (
     [ 'Warning: isMounted(...) is deprecated', 'Module RCTImageLoader' ]
 )
 
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+
+const Users = [
+    {id:1, lemon: {uri:"https://placehold.it/100"} },
+    {id:2, lemon: {uri:"https://placehold.it/200"} },
+    {id:3, lemon: {uri:"https://placehold.it/300"} },
+    {id:4, lemon: {uri:"https://placehold.it/400"} }
+]
+
+export default class App extends Component {
+ 
+ renderUsers = () => {
+
+     return Users.map((item,i) => {
+         return (
+            <Animated.View 
+                key={i}
+                style={{height:SCREEN_HEIGHT-120, width: SCREEN_WIDTH, padding: 10}}>
+                <Image
+                    style={{ flex:1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }} 
+                    source={item.lemon} />
+            </Animated.View>
+         )
+     })
+
+ }
 
 
-const App = () => {
-        console.log('hello, world!')
-        const lemon = [1,2,3,4,5]
-        const lime = {
-            backgroundColor: '#ccc',
-            padding: 10,
-            margin: 10
-        }
-       return <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            {
-                lemon.map((l,i) => <View key={i} style={lime}><Text>{l}</Text></View>)
-            }
-        </View>
-       
+  render() {
+ 
+   return (
+       <View style={{flex:1}}>
+           <View style={{height:60}}>
+           
+           </View>
+
+            <View style={{flex:1}}>
+             {this.renderUsers()}
+            </View>
+
+            <View style={{height:60}}>
+           
+           </View>
+       </View>
+)
+}
 }
 
 
