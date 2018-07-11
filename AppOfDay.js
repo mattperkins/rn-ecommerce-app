b366d22ec63091cc06e1a1f66c3d1e732b7e65f3
mattperkins <github@mattperkins.com>
2018-07-11 12:42:42 +0100

    with local images

--- AppOfDay.js
+++ AppOfDay.js
@@ -1,86 +1,97 @@
 import React from 'react'
-import { View, Image, ScrollView, TouchableWithoutFeedback, Animated, Dimensions, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, Image, ScrollView, TouchableWithoutFeedback, Animated, Dimensions, SafeAreaView, StyleSheet } from 'react-native'
 
 let SCREEN_WIDTH = Dimensions.get('window').width
 let SCREEN_HEIGHT = Dimensions.get('window').height
 const images = [
-    { id: 1, src: "https://picsum.photos/1001" },
-    { id: 2, src: "https://picsum.photos/1002" }
+    // { id: 1, src: "https://picsum.photos/1001" },
+    // { id: 2, src: "https://picsum.photos/1002" }
+    { id: 1, src: require('./assets/1001.jpg') },
+    { id: 2, src: require('./assets/1002.jpg') }
 ]
 
-constructor(props){
-    super(props)
-
-    this.allImages = {}
-    this.oldPosition = {}
-    this.position = new Animated.ValueXY()
-    this.dimensions = new Animated.ValueXY()
-
-    this.set = {
-        activeImage: null
-    }
-}
-
-openImage = (i) => {
-
-    this.allImages[i].measure(x, y, width, height, pageX, pageY) => {
-        this.oldPosition.x = pageX
-        this.oldPosition.y = pageY
-        this.oldPosition.width = width
-        this.oldPosition.height = height
-
-        this.position.setValue({
-            x: pageX,
-            y: pageY
-        })
-
-        this.dimensions.setValue({
-            x: width,
-            y: height
-        })
-
-        this.setState({
-            activeImage: images[i]
-        }, () => {
-            this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
-
-                Animated.parallel([
-                    Animated.timing(this.position.x, {
-                        toValue: dPageX,
-                        duration: 300
-                    }),
-
-
-                    Animated.timing(this.position.y, {
-                        toValue: dPageY,
-                        duration: 300
-                    }),
-
-
-                    Animated.timing(this.dimensions.x, {
-                        toValue: dWidth,
-                        duration: 300
-                    }),
-
-
-                    Animated.timing(this.dimensions.y, {
-                        toValue: dHeight,
-                        duration: 300
-                    })
-                ]).start()
-
-            })
-        })
-    }
-}
-
 export default class AppOfDay extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            activeImage: null
+        }
+
+        this.allImages = {}
+        this.oldPosition = {}
+        this.position = new Animated.ValueXY()
+        this.dimensions = new Animated.ValueXY()
+        this.animation = new Animated.Value(0)
+        this.activeImageStyle = null
+    }
+
+
+    openImage = (i) => {
+
+        this.allImages[i].measure((x, y, width, height, pageX, pageY) => {
+            this.oldPosition.x = pageX
+            this.oldPosition.y = pageY
+            this.oldPosition.width = width
+            this.oldPosition.height = height
+
+            this.position.setValue({
+                x: pageX,
+                y: pageY
+            })
+
+            this.dimensions.setValue({
+                x: width,
+                y: height
+            })
+
+            this.setState({
+                activeImage: images[i]
+            }, () => {
+                this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+
+                    Animated.parallel([
+                        Animated.timing(this.position.x, {
+                            toValue: dPageX,
+                            duration: 300
+                        }),
+
+
+                        Animated.timing(this.position.y, {
+                            toValue: dPageY,
+                            duration: 300
+                        }),
+
+
+                        Animated.timing(this.dimensions.x, {
+                            toValue: dWidth,
+                            duration: 300
+                        }),
+
+
+                        Animated.timing(this.dimensions.y, {
+                            toValue: dHeight,
+                            duration: 300
+                        }),
+
+                        Animated.timing(this.animation, {
+                            toValue: 1,
+                            duration: 300
+                        })
+                    ]).start()
+
+                })
+            })
+        }
+        )
+    }
 
     render() {
 
         const activeImageStyle = {
             width: this.dimensions.x,
-            height: this.dimension.y,
+            height: this.dimensions.y,
             left: this.position.x,
             top: this.position.y
         }
@@ -100,9 +111,10 @@ export default class AppOfDay extends React.Component {
                                 <Animated.View
                                     style={{ height: SCREEN_HEIGHT - 150, width: SCREEN_WIDTH, padding: 15 }}
                                 >
+                                    {/* source={{ uri: image.src }} */}
                                     <Image
                                         ref={(image) => (this.allImages[i] = image)}
-                                        source={{ uri: image.src }} style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 20 }} />
+                                        source={image.src} style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 20 }} />
                                 </Animated.View>
                             </TouchableWithoutFeedback>
                         )
@@ -126,11 +138,14 @@ export default class AppOfDay extends React.Component {
                     </View>
 
 
-                    <View
-                        style={{ flex: 1 }}
+                    <Animated.View
+                        style={{ flex: 1, backgroundColor: 'white', padding: 20, paddingTop: 50 }}
                     >
-
-                    </View>
+                        <Text
+                            style={{ fontSize: 24, paddingBottom: 10 }}
+                        >Title Goes Here</Text>
+                        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, incidunt vitae sapiente neque fugit ex ducimus nesciunt expedita nisi voluptatum sit tenetur in quos doloremque, cupiditate modi. Dignissimos, distinctio magni? </Text>
+                    </Animated.View>
 
                 </View>
 
